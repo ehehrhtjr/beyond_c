3c4d3c4dc2ffcd326541b6eba8a1c6556977a1ae
ehehrhtjr <107744549+ehehrhtjr@users.noreply.github.com>
2022-08-06 14:00:20 +0900

    Update 1789.c

--- 1789.c
+++ 1789.c
@@ -1,17 +1,1 @@
-#define _CRT_SECURE_NO_WARNINGS
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <math.h>
-
-int main() {
-	float n;
-	float S;
-
-	scanf("%f", &S);
-
-	n = (-1 + sqrt(1 + 4 * S *2)) / 2;
-	n = floor(n);
-	printf("%d",(int)n);
-}
+//
